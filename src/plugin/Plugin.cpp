d2ae6933f84a3b4a157fc02511c501b9722d7318
frank <frank@Debian.myguest.virtualbox.org>
2022-12-18 10:28:19 +0100

    add .clang-format

--- src/plugin/Plugin.cpp
+++ src/plugin/Plugin.cpp
@@ -1,8 +1,8 @@
 #include "Plugin.h"
 
-#include<iostream>
+#include <iostream>
 
-Plugin::Plugin( std::string const &_file ) : file(_file)
+Plugin::Plugin( std::string const &_file ) : file( _file )
 {
 }
 
@@ -12,10 +12,10 @@ Plugin::~Plugin()
 
 void Plugin::load()
 {
-	std::cout << "load plugin from file " << file <<std::endl;
+    std::cout << "load plugin from file " << file << std::endl;
 }
 
 void Plugin::close()
 {
-	std::cout << "close plugin from file " << file <<std::endl;
+    std::cout << "close plugin from file " << file << std::endl;
 }
