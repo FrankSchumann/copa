d7ef874ad7b09b7291235e1f3f687b42ee1052df
frank <frank@Debian.myguest.virtualbox.org>
2022-12-19 13:55:25 +0100

    dependency injection of FactoryController into ComponentController

--- src/component/ComponentController.cpp
+++ src/component/ComponentController.cpp
@@ -5,7 +5,11 @@ namespace COPA
 {
 std::map< std::string, std::shared_ptr< ComponentIf > > ComponentController::components;
 
-ComponentController::ComponentController()
+ComponentController::ComponentController(): ComponentController(std::make_shared<FactoryController>())
+{
+}
+
+ComponentController::ComponentController(std::shared_ptr<FactoryControllerIf> const &_factoryController) : factoryController(_factoryController)
 {
 }
 
@@ -15,6 +19,11 @@ ComponentController::~ComponentController()
 
 void ComponentController::create( std::string const &name, std::string const &type )
 {
+	std::shared_ptr<FactoryIf> factory = factoryController->get(type);
+
+	std::shared_ptr<ComponentIf> component;
+
+	factory->create();
 }
 
 std::shared_ptr< ComponentIf > ComponentController::get( std::string const &name )
